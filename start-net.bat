099f3b1bfdd9eb00698db1f9e88610ae597f0023
ZixcIdus <147542662+ZixcIdus@users.noreply.github.com>
2026-01-11 19:33:47 +0500

    Delete start-net.bat

--- start-net.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM === Start AmneziaWG ===
-start "" "C:\Program Files\AmneziaWG\AmneziaWG.exe"
-
-REM === Enable split routing ===
-powershell -NoProfile -ExecutionPolicy Bypass ^
-  -File "%~dp0amnezia-on.ps1"
